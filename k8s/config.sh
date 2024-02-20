18af7c7bf6a7bcee3a8c2880ca1ee8709431ae23
ryan <ryansavordelli@hotmail.com>
2024-02-20 10:28:31 -0300

    teste

--- k8s/config.sh
+++ k8s/config.sh
@@ -4,9 +4,10 @@ export TESTE_SHELL='Variavel teste'
 
 
 testeFunction(){
-    if [ "$ENV_TEST = "DEV" "];then
-        echo 'Ambiente é  '$ENV_TEST''
-    else
-        echo 'Outro ambiente'
-    fi
+    echo $ENV_TEST
+#    if [ "$ENV_TEST = "DEV" "];then
+#        echo 'Ambiente é  '$ENV_TEST''
+#    else
+#        echo 'Outro ambiente'
+#    fi
 }
